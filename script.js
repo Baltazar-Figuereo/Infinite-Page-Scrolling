06d83cb6172d21d6d9ff93f7088204d98dca4f70
Baltazar-Figuereo <Bfiguereo40@gmail.com>
2020-09-16 21:55:26 -0400

    Removed placeholder images in container. Added infinite scroll feature

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0
 let photosArray = [];
 
 const count = 10;
 const apiKey = "rG7g_8Rz15sQ7HpPZ_yYDpBEUfp8cdEJAfyo8xTuJo4";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function imageLoaded() {
+    console.log("imaged loaded")
+    imagesLoaded++;
+
+    if(imagesLoaded === totalImages) {
+        ready = true;
+        console.log()
+    }
+}
+
 function displayPhoto(photosArray) {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    
+    console.log("Total images = ", totalImages);
+
     photosArray.forEach(photo => {
         const item = document.createElement("a");
         item.setAttribute("href", photo.links.html);
@@ -17,6 +35,8 @@ function displayPhoto(photosArray) {
         img.setAttribute("src", photo.urls.regular);
         img.setAttribute("alt", photo.alt_description)
         img.setAttribute("title", photo.alt_description)
+
+        img.addEventListener("load", imageLoaded);
 
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -36,3 +56,12 @@ async function getPhotos() {
     }
 }
 
+
+window.addEventListener("scroll", ()=> {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready ) {
+        ready = false;
+        getPhotos();
+    }
+})
+
+getPhotos();
